5efdda4f301a97db8fbdb6bd1e5ae0e4fbb8f7a4
rpgido <ronald.gido@gmail.com>
2013-11-23 23:17:09 +0800

    DTO and Parser
    DTO and Parser

---
+++ RescuePoint/DTOs/DTOMorgue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class DTOMorgue
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Latitude { get; set; }
+    public string Longtitude { get; set; }
+    public string TotalBodies { get; set; }
+    public string TotalMale { get; set; }
+    public string TotalFemale { get; set; }
+    public string TotalTeen { get; set; }
+    public string TotalAdult { get; set; }
+    public string TotalChild { get; set; }
+    public string ContactNumber { get; set; }
+    public DTOPersonList PersonList { get; set; }
+
+}
+
+public class DTOMorgueList : List<DTOMorgue>, IDisposable
+{
+    public void Dispose()
+    {
+    }
+}
---
+++ RescuePoint/DTOs/DTOPerson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class DTOPerson
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public string AgeBracket { get; set; }
+    public bool Identified { get; set; }
+}
+
+public class DTOPersonList : List<DTOPerson>, IDisposable
+{
+    public void Dispose()
+    {
+    }
+}
