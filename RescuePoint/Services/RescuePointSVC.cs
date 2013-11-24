1cf71ca62ad9afe3fc33b0ad0f4d0e0a9b374f10
Alfred Jett Grandeza <ajgrandeza@gmail.com>
2013-11-24 00:26:25 -0800

    ambot nimo j

---
+++ RescuePoint/Services/RescuePointSVC.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RescuePoint.Services
+{
+    public class RescuePointSVC
+    {
+        public RescuePointSVC()
+        {
+
+        }
+
+        public async void GetCurrentLocation()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void PlotPOIs()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
+    }
+}
